bd78e306cadc8e4740a611b00b417678f037a0df
SteveMB1 <steven@stevenbain.com>
2016-04-07 01:17:21 -0400

    Added a new function to the index, this will allow my team to pull updates from GIT onto the server with a push of a button

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <?php
 echo '<input type="submit" name="submit" value="submit" onclick="updategit()" />';
-function updategit(){shell_exec (string $"git pull team3 master") };
+function updategit(){shell_exec (string "git pull team3 master") };
 ?>
 
 </body>
