851304461b1265bfaac55bc1ac05bd646b01f93a
checkkdahustle <checkkdahustle@fullsail.edu>
2016-04-06 23:31:44 -0400

    Created index, info, and readme file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Team 3</title>
+	</head>
+	<body>
+		<h1>Team Three</h1>
+
+		<
+		<ul>
+			<li></li>
+		</ul>
+
+	</body>
+</html>
